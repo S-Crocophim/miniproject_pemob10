88398657895c3742a1507f3cd81d8bea87077f29
S - Crocophim <56358591+S-Crocophim@users.noreply.github.com>
2025-06-17 14:59:38 +0700

    utils

---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,59 @@
+// lib/utils/app_theme.dart
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFF0D47A1); // Biru Tua
+  static const Color secondaryColor = Color(0xFF42A5F5); // Biru Muda
+  static const Color accentColor = Color(0xFFFFC107); // Kuning
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+  static const Color textColor = Color(0xFF333333);
+
+  static ThemeData get theme {
+    return ThemeData(
+      primaryColor: primaryColor,
+      scaffoldBackgroundColor: backgroundColor,
+      fontFamily:
+          'Poppins', // Pastikan Anda menambahkan font ini di pubspec.yaml
+
+      colorScheme: const ColorScheme.light(
+        primary: primaryColor,
+        secondary: secondaryColor,
+        error: Colors.red,
+        onPrimary: Colors.white,
+      ),
+
+      appBarTheme: const AppBarTheme(
+        backgroundColor: primaryColor,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+
+      buttonTheme: const ButtonThemeData(
+        buttonColor: primaryColor,
+        textTheme: ButtonTextTheme.primary,
+      ),
+
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: primaryColor,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+      ),
+
+      cardTheme: CardThemeData(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+      ),
+    );
+  }
+}
